368a8ce6256308249d6f767455d0e0d7bd369d3a
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-04-08 13:11:12 +1200

    finalised

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function binary(a) {
   var binary = ""; //Prepares variable
-  if (a <= 1024) { //As long as decimal value is less than limit, then proceed
+  if (a <= 1023) { //As long as decimal value is less than limit, then proceed
     while (a > 0) { //Making sure its a valid integer, no negatives or letters
       binary = (a % 2) + binary; //Decimal to binary conversion
       a = Math.floor(a / 2); //Round 'a' down to the nearest figure, once 'a' is at 0, iterations/loop will stop.
@@ -15,7 +15,7 @@ function binary(a) {
 //      Enter Binary Number Here      /
 //************************************/
 
-var binaryNumber = 10; //Enter decimal value here that you would like converted
+var binaryNumber = 1023; //Enter decimal value here that you would like converted
 console.log(binary(binaryNumber));
 
 //************************************/
